c8e3e626453a4d7f78f5956467f71b25309c422f
tezard <tezard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-08 14:45:10 +0000

    expand help files
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/splits@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dimReduct.R
+++ pkg/R/dimReduct.R
@@ -1,13 +1,13 @@
-`dimReduct` <-
-function (XX, how="PA", scale=TRUE) 
+dimReduct <-
+function (XX, how="PA", scale=TRUE, iterations, centile) 
 	{
-	if (!is.matrix(XX)) stop("XX is not a matrix.")
-	if (how!="KG" & how!="PA") stop ("Invalid method to reduce dimensions.")
+	if (!is.matrix(XX)) XX <- as.matrix(XX)
+	if (how!="KG" & how!="PA") stop ("Invalid method to reduce dimensions.\nUse either "KG" or "PA".")
 	
 	m1<-prcomp(XX,scale=scale)
-	if (how=="KG") {nk<-as.numeric(nScree(m1$sdev^2)$Components[4])}
-	if (how=="PA") {nk<- as.numeric(paran(XX, iterations=2000,centile=95,quietly=TRUE)[[1]])}
-	dR <- list(strippedSpace=predict(m1)[,1:nk], nF=nk)
+	if (how=="KG") {nk<-max(which(m1$sdev^2>=1))}
+	if (how=="PA") {nk<- as.numeric(paran(XX, iterations=iterations,centile=centile,quietly=TRUE)[[1]])}
+	dR <- sS=predict(m1)[,1:nk]
 	return(dR)
 	}
 
