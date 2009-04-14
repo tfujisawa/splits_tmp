a3b33b9c7b8d67350826b962b10ebfae27dd9f54
tezard <tezard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-14 22:06:42 +0000

    resolve some frustrating encoding errors
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/splits@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dimReduct.R
+++ pkg/R/dimReduct.R
@@ -1,13 +1,13 @@
-dimReduct <-
+`dimReduct`  <-
 function (XX, how="PA", scale=TRUE, iterations, centile) 
 	{
 	if (!is.matrix(XX)) XX <- as.matrix(XX)
-	if (how!="KG" & how!="PA") stop ("Invalid method to reduce dimensions.\nUse either "KG" or "PA".")
+	if (how!="KG" & how!="PA") stop ("Invalid method to reduce dimensions.\nUse either 'KG' or 'PA'.")
 	
 	m1<-prcomp(XX,scale=scale)
-	if (how=="KG") {nk<-max(which(m1$sdev^2>=1))}
-	if (how=="PA") {nk<- as.numeric(paran(XX, iterations=iterations,centile=centile,quietly=TRUE)[[1]])}
-	dR <- sS=predict(m1)[,1:nk]
+	if (how=="KG") {nk <- max(which(m1$sdev^2>=1))}
+	if (how=="PA") {nk <- as.numeric(paran(XX, iterations=iterations,centile=centile,quietly=TRUE)[[1]])}
+	dR <- predict(m1)[,1:nk]
 	return(dR)
 	}
 
